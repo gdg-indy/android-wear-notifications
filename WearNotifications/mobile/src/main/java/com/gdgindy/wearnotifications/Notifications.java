57714fbdae18db527495a3a94083b71dfdc1409f
Aaron Milam <aaron@aaron-milam.com>
2014-09-14 19:30:41 -0400

    Refactored notification code

--- WearNotifications/mobile/src/main/java/com/gdgindy/wearnotifications/Notifications.java
+++ WearNotifications/mobile/src/main/java/com/gdgindy/wearnotifications/Notifications.java
@@ -16,30 +16,43 @@ public class Notifications {
     public static void showNotification(Context context) {
         notificationId++;
 
-        // Build intent for notification content
-        Intent viewIntent = new Intent(context, MyActivity.class);
-        PendingIntent viewPendingIntent =
-                PendingIntent.getActivity(context, 0, viewIntent, 0);
-
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(context)
                         .setSmallIcon(R.drawable.ic_launcher)
                         .setContentTitle("Notification title")
                         .setContentText("Hey! A notification!")
-                        .setContentIntent(viewPendingIntent)
-                        .setGroup(NOTIFICATION_GROUP)
                 ;
 
+        addPendingIntent(notificationBuilder, context);
+
+        setGroup(notificationBuilder);
+
+        notify(notificationBuilder, context);
+
+        if(notificationId > 1) {
+            showGroupSummaryNotification(context);
+        }
+    }
+
+    private static void notify(NotificationCompat.Builder notificationBuilder, Context context) {
         // Get an instance of the NotificationManager service
         NotificationManagerCompat notificationManager =
                 NotificationManagerCompat.from(context);
 
         // Build the notification and issues it with notification manager.
         notificationManager.notify(notificationId, notificationBuilder.build());
+    }
 
-        if(notificationId > 1) {
-            showGroupSummaryNotification(context);
-        }
+    private static void addPendingIntent(NotificationCompat.Builder notificationBuilder, Context context) {
+        // Build intent for notification content
+        Intent viewIntent = new Intent(context, MyActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, viewIntent, 0);
+
+        notificationBuilder.setContentIntent(pendingIntent);
+    }
+
+    private static void setGroup(NotificationCompat.Builder notificationBuilder) {
+        notificationBuilder.setGroup(NOTIFICATION_GROUP);
     }
 
     private static void showGroupSummaryNotification(Context context) {
