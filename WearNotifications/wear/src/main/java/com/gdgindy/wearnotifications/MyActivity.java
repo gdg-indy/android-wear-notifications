9c2f16738353c5f5201a087cac69184451067fb2
Aaron Milam <aaron@aaron-milam.com>
2014-09-16 07:34:14 -0400

    Added free-form speech input example

--- WearNotifications/wear/src/main/java/com/gdgindy/wearnotifications/MyActivity.java
+++ WearNotifications/wear/src/main/java/com/gdgindy/wearnotifications/MyActivity.java
@@ -1,9 +1,14 @@
 package com.gdgindy.wearnotifications;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class MyActivity extends Activity {
 
@@ -18,7 +23,41 @@ public class MyActivity extends Activity {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
+                mTextView.setOnClickListener(onViewClickListener);
             }
         });
+    }
+
+    private View.OnClickListener onViewClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            displaySpeechRecognizer();
+        }
+    };
+
+    private static final int SPEECH_REQUEST_CODE = 199;
+
+    // Create an intent that can start the Speech Recognizer activity
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+        // Start the activity, the intent will be populated with the speech text
+        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    }
+
+    // This callback is invoked when the Speech Recognizer returns.
+    // This is where you process the intent and extract the speech text from the intent.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+
+            // Do something with spokenText
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
